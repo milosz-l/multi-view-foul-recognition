010dbd820eef585757650ed7cad8fcf64f1a4538
milosz-l <milosz.lopatto@gmail.com>
2024-05-30 17:52:09 +0200

    269: more augmentation

--- src/augment.py
+++ src/augment.py
@@ -11,6 +11,8 @@ def get_augmentation(data_aug: bool):
                                         transforms.ColorJitter(brightness=0.6, saturation=0.6, contrast=0.6),
                                         transforms.RandomHorizontalFlip(),
                                         transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+                                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                        transforms.RandomGrayscale(p=0.1),
                                         # transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value='random')
         ])
     else:
