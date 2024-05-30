00453a6eb84f379c53da859dab7bf5dda9375b2a
milosz-l <milosz.lopatto@gmail.com>
2024-05-30 13:35:14 +0000

    265: frame rate in the middle with better augmentation

--- src/augment.py
+++ src/augment.py
@@ -4,15 +4,15 @@ def get_augmentation(data_aug: bool):
     if data_aug:
         print("Using data augmentation")
         transformAug = transforms.Compose([
-                                        transforms.RandomAffine(
-                                            degrees=(0, 0), translate=(0.1, 0.1), scale=(0.9, 1)),
-                                        transforms.RandomPerspective(
-                                            distortion_scale=0.3, p=0.5),
-                                        transforms.RandomRotation(degrees=5),
-                                        transforms.ColorJitter(
-                                            brightness=0.5, saturation=0.5, contrast=0.5),
-                                        transforms.RandomHorizontalFlip()
-                                        ])
+                                        transforms.RandomResizedCrop(size=(224, 224), scale=(0.8, 1.0)),
+                                        transforms.RandomAffine(degrees=(0, 0), translate=(0.2, 0.2), scale=(0.8, 1.2)),
+                                        transforms.RandomPerspective(distortion_scale=0.5, p=0.5),
+                                        transforms.RandomRotation(degrees=10),
+                                        transforms.ColorJitter(brightness=0.6, saturation=0.6, contrast=0.6),
+                                        transforms.RandomHorizontalFlip(),
+                                        transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+                                        # transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value='random')
+        ])
     else:
         print("Not using data augmentation")
         transformAug = None
